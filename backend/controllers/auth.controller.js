eb51413003961cb30e4c983b7266eacfd64d04bb
Paras Puru <paraskmahto@gmail.com>
2024-06-21 01:16:52 +0530

    hashed password changes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,13 @@
 const User = require("../models/user.model.js");
+const bcryptjs = require("bcryptjs");
 
 const handleSignupController = async (req, res) => {
   try {
     const { username, email, password } = req.body;
+    const hashedPassword = await bcryptjs.hashSync(password, 10);
     console.log("Request Body", req.body);
     console.log("POST Request -- ", username, email, password);
-    const newUser = new User({ username, email, password });
+    const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
     res.status(201).json({ message: "User created successfully." });
   } catch (error) {
